d2b53a2927c2c988df1570ace793d32626c1b6e5
chen1008 <jack20051008@gmail.com>
2024-12-03 21:21:16 +0800

    update

--- GCD_linear_combination.py
+++ GCD_linear_combination.py
@@ -5,7 +5,7 @@ def extended_gcd(a, b):
     x = y1
     y = x1 - (a // b) * y1
     return gcd, x, y
-input_list=list(map(int,input("please type all element in the same line.").split()))
+input_list=list(map(int,input("Enter all elements in one line.").split()))
 coefficient=[]
 gcd=input_list[0]
 for i in input_list:
@@ -13,9 +13,9 @@ for i in input_list:
     coefficient=[a*x for a in coefficient]
     coefficient.append(y)
 print(f"GCD of {', '.join(map(str,input_list))} is {gcd}")
-print("GCD linear combination is",end='')
+print("The linear combination for the GCD is",end='')
 for i, (a, b) in enumerate(zip(coefficient, input_list)):
-    if i > 0:  # 從第二個元素開始，前面要加上 + 或 - 符號
+    if i > 0: 
         if a > 0:
             print(f"+ {a} * {b} ", end='')
         else:
